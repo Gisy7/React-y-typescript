73b41a4e7341a6b56100cbf1b5c119f214a36ea0
Gisy7 <gisyestevezfp@gmail.com>
2024-12-15 00:07:52 +0000

    proyecto node y express

--- rest_api_node_ts_server/src/server.ts
+++ rest_api_node_ts_server/src/server.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import router from "./router";
+import db from "./config/db"
+import colors from "colors"
 
+// Conectar a la base de datos
+async function connectDb() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log(colors.bgGreen.white.bold("conexion existosa"));
+    } catch (error) {
+        console.log(error);
+        console.log(colors.bgRed.white("se ha producido un error a la hora de conectar a la base de datos"));
+    }
+}
+
+connectDb()
+
+
+// instancia de express
 const server = express()
 
-// Routing
-server.get("/", (req, res) => {
-    console.log("HOLA")
+// Leer datos de formularios
+server.use(express.json())
 
-    res.send("Hola mundo")
-})
+// Rutas de la api
+server.use("/api", router)
+
 
 export default server
